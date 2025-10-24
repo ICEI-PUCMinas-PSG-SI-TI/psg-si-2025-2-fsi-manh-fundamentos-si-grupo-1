c06bde55533a090c6aaadb8a9530bc8dd70715c3
mateusjdev <git@mateusj.dev>
2025-10-24 20:47:36 -0300

    feat: Adicionado repositório de configurações

---
+++ src/backend/src/repository/repositorioConfiguracoes.ts
@@ -0,0 +1,70 @@
+import { eq } from "drizzle-orm";
+import baseDados from "../db";
+import {
+  configuracoesTable,
+  type InsertConfiguracaoSchema,
+  type SelectConfiguracaoSchema,
+  type UpdateConfiguracaoSchema,
+} from "../db/schema/configuracoes";
+
+export class RepositorioCategorias {
+  inserir(
+    configuracoes: InsertConfiguracaoSchema,
+  ): Promise<bigint | undefined> {
+    return baseDados.transaction(async (tx) => {
+      const resultSet = await tx
+        .insert(configuracoesTable)
+        .values(configuracoes);
+      return resultSet.lastInsertRowid;
+    });
+  }
+
+  selecionarPorId(id: string): Promise<SelectConfiguracaoSchema[]> {
+    return baseDados.transaction((tx) => {
+      return tx
+        .select()
+        .from(configuracoesTable)
+        .where(eq(configuracoesTable.id, id));
+    });
+  }
+
+  selecionarTodos(
+    page: number = 1,
+    pageSize: number = 10,
+  ): Promise<SelectConfiguracaoSchema[]> {
+    return baseDados.transaction((tx) => {
+      if (page >= 1 && pageSize >= 1) {
+        return tx
+          .select()
+          .from(configuracoesTable)
+          .limit(pageSize)
+          .offset((page - 1) * pageSize);
+      } else {
+        return tx.select().from(configuracoesTable);
+      }
+    });
+  }
+
+  atualizarPorId(
+    id: string,
+    configuracoes: UpdateConfiguracaoSchema,
+  ): Promise<number> {
+    return baseDados.transaction(async (tx) => {
+      const resultSet = await tx
+        .update(configuracoesTable)
+        .set(configuracoes)
+        .where(eq(configuracoesTable.id, id));
+      return resultSet.rowsAffected;
+    });
+  }
+
+  // or .returning()
+  excluirPorId(id: string) {
+    return baseDados.transaction(async (tx) => {
+      const resultSet = await tx
+        .delete(configuracoesTable)
+        .where(eq(configuracoesTable.id, id));
+      return resultSet.rowsAffected;
+    });
+  }
+}
