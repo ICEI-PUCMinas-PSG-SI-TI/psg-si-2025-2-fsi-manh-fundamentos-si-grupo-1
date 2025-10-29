4836094cdefe11a6e71309c0837813e9aacd4a39
Lucas Loiolla Gomes <loiollalucas@gmail.com>
2025-10-29 14:18:36 -0300

    alterações1

--- src/frontend/src/components/login/LoginForm.vue
+++ src/frontend/src/components/login/LoginForm.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="relative flex justify-center items-center w-screen h-screen overflow-hidden p-4">
-    <div class="relative w-full max-w-md sm:max-w-lg lg:max-w-xl">
-      <logo-login-item class="absolute -top-5 left-1/2 transform -translate-x-1/2 w-30 h-45 sm:w-32 sm:w-45 md:w-42 md:h-45" />
+    <div class="relative w-full max-w-md sm:max-w-lg lg:max-w-xl xl:-mt-16">
+      <logo-login-item class="  absolute -top-5 left-1/2 transform -translate-x-1/2 w-30 h-45 sm:w-32 sm:w-45 md:w-42 md:h-45"  />
 
       <!-- Formulário -->
       <div class="flex justify-center">
         <form @submit.prevent ="login"
-          class="w-100 h-100 pt-25 mt-20 bg-gray-300 bg-opacity-90 rounded- shadow-xl p-8 flex flex-col gap-4 text-white"
+          class="w-105 h-100 pt-25 mt-20 bg-gray-300 bg-opacity-90 rounded-3xl shadow-xl p-8 flex flex-col gap-4 text-white"
         >
           <!-- Usuário -->
 
           <div
-            class="flex items-center bg-gray-700 px-3 py-2 focus-within:ring-2 focus-within:ring-blue-400 transition"
+            class="flex items-center bg-neutral-600  rounded- px-3 py-2 focus-within:rin  transition"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -19,7 +19,7 @@
               viewBox="0 0 24 24"
               stroke-width="1.5"
               stroke="currentColor"
-              class="w-6 h-6 mr-2 text-white"
+              class="w-6 h-7 mr-2 text-white"
             >
               <path
                 stroke-linecap="round"
@@ -38,7 +38,7 @@
 
           <!-- Senha -->
           <div
-            class="flex items-center bg-gray-700 px-3 py-2 focus-within:ring-2 focus-within:ring-blue-400 transition"
+            class="flex items-center bg-neutral-600  px-3 py-2 focus-within:rin  transition"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -46,7 +46,7 @@
               viewBox="0 0 24 24"
               stroke-width="1.5"
               stroke="currentColor"
-              class="w-6 h-6 mr-2 text-white"
+              class="w-6 h-7 mr-2 text-white"
             >
               <path
                 stroke-linecap="round"
@@ -75,7 +75,7 @@
           <!-- Botão -->
           <button
             type="submit"
-            class="cursor-pointer mt-4 w-full bg-purple-700 hover:bg-purple-800 text-white font-semibold py-2 transition transform hover:scale-105"
+            class="shadow-xl cursor-pointer mt-4 w-full bg-rose-600  text-white font-bold py-2 transition transform hover:scale-105"
             >
             Login
           </button>
@@ -89,7 +89,9 @@
 <script setup lang="ts">
 import LogoLoginItem from './LogoLoginItem.vue'
 import { ref } from 'vue'
+import { useRouter } from 'vue-router'
 
+const router = useRouter()
 const usuario = ref('')
 const senha = ref('')
 const erro = ref('')
@@ -102,8 +104,15 @@ interface Usuario{
   salted_password: string;
   [key: string]: string;
 }
+function validarEmail(email:string):boolean{
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+  return regex.test(email)
+}
 function login() {
-
+  if(!validarEmail(usuario.value)){
+    erro.value = 'Digite um email válido'
+    return;
+  }
   fetch('http://localhost:3000/usuarios')
     .then(res => {
       if (!res.ok) throw new Error('Não foi possível carregar os usuários')
@@ -116,6 +125,8 @@ function login() {
       if (encontrado) {
         erro.value = ''
         alert(`Bem-vindo, ${encontrado.nome}!`)
+        router.push('/dashboard')
+
       } else {
         erro.value = 'Login ou senha incorretos'
         usuario.value = ''
