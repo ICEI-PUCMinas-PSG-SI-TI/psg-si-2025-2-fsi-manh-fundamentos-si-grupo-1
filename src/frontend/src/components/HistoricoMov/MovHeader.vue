df99c2a8825b9e786bce2fb5f84b71dc5c7a8593
Lucas Loiolla Gomes <loiollalucas@gmail.com>
2025-10-30 18:31:46 -0300

    criacao-tabela-movimentacoes

---
+++ src/frontend/src/components/HistoricoMov/MovHeader.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="bg-base-200 flex items-center justify-between px-7 py-3">
+      <h1 class="font-bold text-5xl">Histórico de Movimentações</h1>
+    </div>
+</template>
--- src/frontend/src/views/MovimentacoesView.vue
+++ src/frontend/src/views/MovimentacoesView.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="w-full h-full m-auto justify-center items-center text-center">
-    <p>Movimentações</p>
+  <div class="w-full h-screen flex flex-col">
+    <MovHeader />
+    <div class="flex-1 overflow-x-auto overflow-y-auto p-6">
+      <MovTabela />
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import MovHeader from '@/components/HistoricoMov/MovHeader.vue';
+import MovTabela from '@/components/HistoricoMov/MovTabela.vue';
+</script>
