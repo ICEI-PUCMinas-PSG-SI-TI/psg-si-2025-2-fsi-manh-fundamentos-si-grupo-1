b7c08358692878849123bb44efa827c76386c3cc
mateusjdev <git@mateusj.dev>
2025-11-06 19:47:49 -0300

    chore: add regras ao front/eslint

--- src/frontend/eslint.config.ts
+++ src/frontend/eslint.config.ts
@@ -19,4 +19,10 @@ export default defineConfigWithVueTs(
   pluginVue.configs['flat/essential'],
   vueTsConfigs.recommended,
   skipFormatting,
+  {
+    rules: {
+      '@typescript-eslint/no-unnecessary-type-assertion': 'error',
+      '@typescript-eslint/no-unnecessary-condition': 'error',
+    },
+  },
 )
