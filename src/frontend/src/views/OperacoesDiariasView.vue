dc599e4540d71420c507a98d5c7c87c543991d21
Lucas Loiolla Gomes <loiollalucas@gmail.com>
2025-12-01 19:16:23 -0300

    função de buscar produto pelo nome e alterações na tabela

--- src/frontend/src/views/OperacoesDiariasView.vue
+++ src/frontend/src/views/OperacoesDiariasView.vue
@@ -1,146 +1,178 @@
-<template>
-  <div class="w-full min-h-screen max-w-full">
-    <div class="bg-base-200 md:m-9 mt-4 p-4 rounded-2xl flex flex-col">
-      <!--Compras, vendas e filtrar por data-->
-      <div class="flex flex-wrap gap-6 mb-6 items-start">
-        <!-- Select de Motivo -->
-        <div class="flex flex-col flex-1 min-w-[200px]">
-          <label class="text-sm font-medium mb-2">Tipo de Movimentação</label>
-          <select
-            class="cursor-pointer select select-bordered w-full bg-white text-black rounded-lg font-medium focus:outline-none focus:ring-2 focus:ring-primary"
-            v-model="motivo"
-            @change="resetarPagina()"
-          >
-            <option disabled selected>Selecione um tipo</option>
-            <option :value="null" selected>Todos</option>
-            <option :value="MotivoTransacoes.Compra">{{ MotivoTransacoes.Compra }}</option>
-            <option :value="MotivoTransacoes.Venda">{{ MotivoTransacoes.Venda }}</option>
-            <option :value="MotivoTransacoes.Devolucao">{{ MotivoTransacoes.Devolucao }}</option>
-            <option :value="MotivoTransacoes.Perda">{{ MotivoTransacoes.Perda }}</option>
-            <option :value="MotivoTransacoes.Transferencia">
-              {{ MotivoTransacoes.Transferencia }}
-            </option>
-          </select>
-        </div>
-
-        <!-- Filtro de Data -->
-        <div class="flex flex-col flex-1 min-w-[200px] bg-stone-700 p-4 rounded-xl">
-          <label class="text-sm font-medium mb-3 text-white">Filtrar por data</label>
-          <div class="flex items-center gap-4 w-full">
-            <input
-              type="checkbox"
-              v-model="checkBoxFiltro"
-              @change="resetarPagina"
-              class="checkbox h-6 w-6 rounded-md border-2 border-white bg-stone-600 checked:bg-green-500 checked:border-green-400 transition-all duration-110"
-            />
-            <input
-              type="date"
-              v-model="dataSelecionada"
-              @change="carregarMovimentacoesDoDia"
-              class="input input-bordered w-full px-3 py-2 px-3 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-primary"
-            />
-          </div>
-        </div>
-      </div>
-      <!--div buscar produto, nova movimentação e histórico-->
-      <div class="bg-stone-700 rounded-2xl p-4 flex flex-col flex-1 min-h-[60vh]">
-        <!-- Busca + Botão Nova Movimentação -->
-        <div class="flex flex-wrap gap-4 justify-between items-center mb-6">
-          <!-- Campo de busca -->
-          <div class="relative w-full md:w-80">
-            <input
-              type="text"
-              v-model="produtoFiltro"
-              @input="carregarMovimentacoesDoDia"
-              placeholder="BUSCAR PRODUTO..."
-              class="w-full px-10 py-2 rounded-3xl bg-white text-black focus:outline-none"
-            />
-            <MagnifyingGlassIcon class="w-6 text-black absolute left-2 top-1/2 -translate-y-1/2" />
+  <template>
+    <div class="w-full min-h-screen max-w-full">
+      <div class="bg-base-200 md:m-9 mt-4 p-4 rounded-2xl flex flex-col">
+        <!--Compras, vendas e filtrar por data-->
+        <div class="flex flex-wrap gap-6 mb-6 items-start">
+          <!-- Select de Motivo -->
+          <div class="flex flex-col flex-1 min-w-[200px]">
+            <label class="text-sm font-medium mb-2">Tipo de Movimentação</label>
+            <select
+              class="cursor-pointer select select-bordered w-full bg-white text-black rounded-lg font-medium focus:outline-none focus:ring-2 focus:ring-primary"
+              v-model="motivo"
+              @change="resetarPagina()"
+            >
+              <option disabled selected>Selecione um tipo</option>
+              <option :value="null" selected>Todos</option>
+              <option :value="MotivoTransacoes.Compra">{{ MotivoTransacoes.Compra }}</option>
+              <option :value="MotivoTransacoes.Venda">{{ MotivoTransacoes.Venda }}</option>
+              <option :value="MotivoTransacoes.Devolucao">{{ MotivoTransacoes.Devolucao }}</option>
+              <option :value="MotivoTransacoes.Perda">{{ MotivoTransacoes.Perda }}</option>
+              <option :value="MotivoTransacoes.Transferencia">
+                {{ MotivoTransacoes.Transferencia }}
+              </option>
+            </select>
           </div>
 
-          <!-- Botão Nova Movimentação -->
-          <button
-            class="cursor-pointer bg-white w-full md:w-60 flex justify-center text-black font-semibold px-4 py-2 rounded-xl transition-transform duration-100 hover:scale-[1.03]"
-            @click="novaMovimentacao = true"
-          >
-            <PlusIcon class="w-5 mr-2"></PlusIcon> NOVA MOVIMENTAÇÃO
-          </button>
+          <!-- Filtro de Data -->
+          <div class="flex flex-col flex-1 min-w-[200px]  ">
+            <label class="text-sm font-medium mb-3 ">Filtrar por data</label>
+            <div class="flex items-center gap-4 w-full">
+              <input
+                type="checkbox"
+                v-model="checkBoxFiltro"
+                @change="resetarPagina"
+                class="checkbox h-6 w-6 rounded-md border-2   checked:bg-green-500 checked:border-green-400 transition-all duration-110"
+              />
+              <input
+                type="date"
+                v-model="dataSelecionada"
+                @change="carregarMovimentacoesDoDia"
+                class="input input-bordered w-full px-3 py-2 px-3 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-primary"
+              />
+            </div>
+          </div>
         </div>
+        <!--div buscar produto, nova movimentação e histórico-->
+        <div class=" rounded-2xl p-4 flex flex-col flex-1 min-h-[60vh]">
+          <!-- Busca + Botão Nova Movimentação -->
+          <div class="flex flex-wrap gap-4 justify-between items-center mb-6">
+            <!-- Campo de busca -->
+            <div class="relative w-full md:w-80">
+              <input
+                type="text"
+                v-model="produtoFiltro"
+                @input="carregarMovimentacoesDoDia"
+                placeholder="BUSCAR PRODUTO..."
+                class="w-full px-10 py-2 rounded-3xl bg-white text-black focus:outline-none"
+              />
+              <MagnifyingGlassIcon class="w-6 text-black absolute left-2 top-1/2 -translate-y-1/2" />
+            </div>
 
-        <!-- TABELA  -->
-        <h3 class="text-3xl font-semibold mb-4 text-white">Histórico</h3>
-        <div class="overflow-x-auto overflow-y-hidden flex-1">
-          <table class="min-w-full text-left border-t border-white">
-            <thead class="sticky top-0 z-10 text-white bg-stone-700">
-              <tr class="text-white text-xl">
-                <th class="py-3 px-2">Data/Hora</th>
-                <th class="py-3 px-2">Tipo</th>
-                <th class="py-3 px-2">Produto</th>
-                <th class="py-3 px-2">Qtd</th>
-                <th class="py-3 px-2">Usuário</th>
-              </tr>
-            </thead>
+            <!-- Botão Nova Movimentação -->
+            <button
+              class="cursor-pointer bg-white w-full md:w-60 flex justify-center text-black font-semibold px-4 py-2 rounded-xl transition-transform duration-100 hover:scale-[1.03]"
+              @click="novaMovimentacao = true"
+            >
+              <PlusIcon class="w-5 mr-2"></PlusIcon> NOVA MOVIMENTAÇÃO
+            </button>
+          </div>
 
-            <tbody>
-              <tr
-                v-for="i in movimentacoes"
-                :key="i.id"
-                class="text-xl text-white border-b border-stone-500"
-              >
-                <td class="py-2 px-2">
-                  {{
-                    new Date(i.horario).toLocaleDateString('pt-BR', {
-                      day: '2-digit',
-                      month: '2-digit',
-                      year: '2-digit',
-                    })
-                  }}
-                  <br />
-                  {{
-                    new Date(i.horario).toLocaleTimeString('pt-BR', {
-                      hour: '2-digit',
-                      minute: '2-digit',
-                    })
-                  }}
-                </td>
-                <td class="py-2 px-2">{{ i.motivo }}</td>
-                <td class="py-2">
-                  {{ i._produto.nome }} <br />
-                  <span class="badge badge-sm badge-primary">{{ i._produto.codigo }}</span>
-                </td>
-                <td class="py-2">{{ i.quantidade }}</td>
-                <td class="py-2">{{ i._usuario.nome }}</td>
-              </tr>
-              <tr v-if="movimentacoes.length === 0">
-                <td colspan="5" class="py-4 text-center text-white">
-                  Nenhuma movimentação encontrada
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-        <div class="flex justify-center mt-4 lg:justify-end items-center gap-2 join">
-          <button
-            class="join-item btn btn-neutral"
-            :class="pagina === 1 ? 'btn-disabled' : 'border-base-content'"
-            @click="pagAnterior"
-          >
-            Anterior
-          </button>
-          <button class="join-item btn btn-neutral border-base-content">{{ pagina }}</button>
-          <button
-            :class="movimentacoes.length < paginaTamanho ? 'btn-disabled' : 'border-base-content'"
-            class="join-item btn btn-neutral"
-            @click="proxPagina"
-          >
-            Próximo
-          </button>
+          <!-- TABELA  -->
+          <h3 class="text-3xl font-semibold mb-4 ">Histórico</h3>
+          <div class="w-full overflow-x-auto rounded-xl ">
+            <table class="min-w-full text-left divide-y divide-white/20">
+              <thead class=" ">
+                <tr class="text-xl">
+                  <th class="p-3">Data/Hora</th>
+                  <th class="p-3">Motivo</th>
+                  <th class="p-3">Produto</th>
+                  <th class="p-3" v-if="motivo === MotivoTransacoes.Transferencia">Origem</th>
+                  <th class="p-3" v-if="motivo === MotivoTransacoes.Transferencia">Destino</th>
+                  <th class="p-3">Qtd</th>
+                  <th class="p-3">Usuário</th>
+                </tr>
+              </thead>
+
+              <tbody class="divide-y divide-white/10">
+                <tr
+                  v-for="i in movimentacoes"
+                  :key="i.id"
+                  class=" text-lg "
+                >
+                  <td class="py-2 px-2">
+                    {{
+                      new Date(i.horario).toLocaleDateString('pt-BR', {
+                        day: '2-digit',
+                        month: '2-digit',
+                        year: '2-digit',
+                      })
+                    }}
+                    <br />
+                    <span class="opacity-80">
+                      {{
+                        new Date(i.horario).toLocaleTimeString('pt-BR', {
+                          hour: '2-digit',
+                          minute: '2-digit',
+                        })
+                      }}
+                    </span>
+                  </td>
+
+                  <td class="p-3 font-bold" :class="{
+                    'text-red-500':i.motivo=== 'Perda' || i.motivo==='Venda',
+                    'text-blue-500':i.motivo==='Transferência',
+                    'text-green-500':i.motivo==='Compra' || i.motivo === 'Devolução'
+                  }">{{ i.motivo }}</td>
+
+                  <td class="p-3">
+                    <span class="font-medium">{{ i._produto.nome }}</span>
+                    <br />
+                    <span class="badge badge-sm badge-primary mt-1">{{ i._produto.codigo }}</span>
+                  </td>
+
+                  <td class="p-3" v-if="motivo === MotivoTransacoes.Transferencia">
+                    <span class="opacity-90">{{ i.localOrigem }}</span>
+                  </td>
+
+                  <td class="p-3" v-if="motivo === MotivoTransacoes.Transferencia">
+                    <span class="opacity-90">{{ i.localDestino }}</span>
+                  </td>
+                  <td class="p-3 font-semibold "
+                  :class="{
+                    'text-red-500':i.motivo=== 'Perda' || i.motivo==='Venda',
+                    'text-blue-500':i.motivo==='Transferência',
+                    'text-green-500':i.motivo==='Compra' || i.motivo === 'Devolução'
+                  }">{{ i.quantidade }}</td>
+
+                  <td class="p-3 whitespace-nowrap">
+                    <div class="flex items-center gap-2">
+                      <div class="w-8 h-8 rounded-full bg-stone-300 flex items-center justify-center text-black">
+                        {{ i._usuario.nome[0].toUpperCase() }}
+                      </div>
+                      {{ i._usuario.nome }}
+                    </div>
+                  </td>
+                </tr>
+                <tr v-if="movimentacoes.length === 0">
+                  <td colspan="7" class="p-4 text-center  opacity-60">
+                    Nenhuma movimentação encontrada
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div class="flex justify-center mt-4 lg:justify-end items-center gap-2 join">
+            <button
+              class="join-item btn btn-neutral"
+              :class="pagina === 1 ? 'btn-disabled' : 'border-base-content'"
+              @click="pagAnterior"
+            >
+              Anterior
+            </button>
+            <button class="join-item btn btn-neutral border-base-content">{{ pagina }}</button>
+            <button
+              :class="movimentacoes.length < paginaTamanho ? 'btn-disabled' : 'border-base-content'"
+              class="join-item btn btn-neutral"
+              @click="proxPagina"
+            >
+              Próximo
+            </button>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-  <NovaMoviment v-if="novaMovimentacao" v-model="novaMovimentacao"></NovaMoviment>
-</template>
+    <NovaMoviment v-if="novaMovimentacao" v-model="novaMovimentacao"></NovaMoviment>
+  </template>
 
 <script setup lang="ts">
 import { ApiMovimentacoes } from '@/api/movimentacoes'
@@ -148,7 +180,7 @@ import { ApiMovimentacoes } from '@/api/movimentacoes'
 import { MagnifyingGlassIcon, PlusIcon } from '@heroicons/vue/24/outline'
 import NovaMoviment from '@/components/OpDiarias/NovaMoviment.vue'
 import type { GetConsultaMovimentacaoDto } from '../../../backend'
-import { onMounted, ref } from 'vue'
+import { onMounted, ref, watch } from 'vue'
 import { MotivoTransacoes, type ConsultaMovimentacoesParams } from '../../../backend'
 
 const motivo = ref<MotivoTransacoes | null>(null)
@@ -160,6 +192,12 @@ const pagina = ref(1)
 const paginaTamanho = ref(30)
 const checkBoxFiltro = ref<boolean>(false)
 const movimentacoes = ref<GetConsultaMovimentacaoDto[]>([])
+
+let searchInterval: NodeJS.Timeout | null = null
+watch(produtoFiltro, () => {
+  if (searchInterval) clearTimeout(searchInterval)
+  searchInterval = setTimeout(carregarMovimentacoesDoDia, 200)
+})
 
 async function carregarMovimentacoesDoDia() {
   const filtros = {
@@ -179,7 +217,13 @@ async function carregarMovimentacoesDoDia() {
   const res = await api.obterTodos(filtros)
 
   if (res.ok && res.responseBody) {
-    movimentacoes.value = res.responseBody
+    let lista = res.responseBody
+
+    if (produtoFiltro.value && produtoFiltro.value.trim() !== '') {
+      const termo = produtoFiltro.value.trim().toLowerCase()
+      lista = lista.filter((mov) => mov._produto.nome.toLowerCase().includes(termo))
+    }
+    movimentacoes.value = lista
   }
 }
 
